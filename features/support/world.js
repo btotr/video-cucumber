535145e3965d436e8aea123c8568eae74f50008d
root <c.meerveld@activevideo.com>
2015-10-07 06:48:38 -0400

    added local chrome option

--- features/support/world.js
+++ features/support/world.js
@@ -1,6 +1,8 @@
 var webdriverio = require('webdriverio');
 
 var url = "http://188.226.244.202:8000/src/";
+
+// saucelabs
 var options = {
     desiredCapabilities: {
         browserName: 'chrome'
@@ -10,6 +12,18 @@ var options = {
     user: process.env.SAUCE_USERNAME,
     key: process.env.SAUCE_ACCESS_KEY,
     logLevel: 'silents'
+}
+
+// local chrome use ssh tunnel and start driver with:
+// chromedriver --whitelisted-ips 188.226.244.202 --url-base=/wd/hub
+// ssh -R *:9999:localhost:9515 root@188.226.244.202
+var options = {
+    desiredCapabilities: {
+        browserName: 'chrome'
+    },
+    host: 'localhost',
+    port: 9999,
+    logLevel: 'verbose'
 }
 
 var World = function World(done) {
